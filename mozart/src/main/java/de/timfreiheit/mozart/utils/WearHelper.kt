3d73a2ecf907cb45bf48e2ec26a13dbf780e196d
Tim Freiheit <timfreiheit@hotmail.de>
2017-10-23 23:22:33 +0200

    avoid compiling dependency for constants

--- mozart/src/main/java/de/timfreiheit/mozart/utils/WearHelper.kt
+++ mozart/src/main/java/de/timfreiheit/mozart/utils/WearHelper.kt
@@ -1,10 +1,18 @@
 package de.timfreiheit.mozart.utils
 
 import android.os.Bundle
-import android.support.wearable.media.MediaControlConstants
 
 internal object WearHelper {
-    private val WEAR_APP_PACKAGE_NAME = "com.google.android.wearable.app"
+
+    /**
+     * @see [https://developer.android.com/reference/android/support/wearable/media/MediaControlConstants.html]
+     */
+    private const val EXTRA_CUSTOM_ACTION_SHOW_ON_WEAR = "android.support.wearable.media.extra.CUSTOM_ACTION_SHOW_ON_WEAR"
+    private const val EXTRA_BACKGROUND_COLOR_FROM_THEME = "android.support.wearable.media.extra.BACKGROUND_COLOR_FROM_THEME"
+    private const val EXTRA_RESERVE_SLOT_SKIP_TO_PREVIOUS = "android.support.wearable.media.extra.RESERVE_SLOT_SKIP_TO_PREVIOUS"
+    private const val EXTRA_RESERVE_SLOT_SKIP_TO_NEXT = "android.support.wearable.media.extra.RESERVE_SLOT_SKIP_TO_NEXT"
+
+    private const val WEAR_APP_PACKAGE_NAME = "com.google.android.wearable.app"
 
     fun isValidWearCompanionPackage(packageName: String): Boolean {
         return WEAR_APP_PACKAGE_NAME == packageName
@@ -13,31 +21,31 @@ internal object WearHelper {
     fun setShowCustomActionOnWear(customActionExtras: Bundle, showOnWear: Boolean) {
         if (showOnWear) {
             customActionExtras.putBoolean(
-                    MediaControlConstants.EXTRA_CUSTOM_ACTION_SHOW_ON_WEAR, true)
+                    EXTRA_CUSTOM_ACTION_SHOW_ON_WEAR, true)
         } else {
-            customActionExtras.remove(MediaControlConstants.EXTRA_CUSTOM_ACTION_SHOW_ON_WEAR)
+            customActionExtras.remove(EXTRA_CUSTOM_ACTION_SHOW_ON_WEAR)
         }
     }
 
     fun setUseBackgroundFromTheme(extras: Bundle, useBgFromTheme: Boolean) {
         if (useBgFromTheme) {
-            extras.putBoolean(MediaControlConstants.EXTRA_BACKGROUND_COLOR_FROM_THEME, true)
+            extras.putBoolean(EXTRA_BACKGROUND_COLOR_FROM_THEME, true)
         } else {
-            extras.remove(MediaControlConstants.EXTRA_BACKGROUND_COLOR_FROM_THEME)
+            extras.remove(EXTRA_BACKGROUND_COLOR_FROM_THEME)
         }
     }
 
     fun setSlotReservationFlags(extras: Bundle, reserveSkipToNextSlot: Boolean,
                                 reserveSkipToPrevSlot: Boolean) {
         if (reserveSkipToPrevSlot) {
-            extras.putBoolean(MediaControlConstants.EXTRA_RESERVE_SLOT_SKIP_TO_PREVIOUS, true)
+            extras.putBoolean(EXTRA_RESERVE_SLOT_SKIP_TO_PREVIOUS, true)
         } else {
-            extras.remove(MediaControlConstants.EXTRA_RESERVE_SLOT_SKIP_TO_PREVIOUS)
+            extras.remove(EXTRA_RESERVE_SLOT_SKIP_TO_PREVIOUS)
         }
         if (reserveSkipToNextSlot) {
-            extras.putBoolean(MediaControlConstants.EXTRA_RESERVE_SLOT_SKIP_TO_NEXT, true)
+            extras.putBoolean(EXTRA_RESERVE_SLOT_SKIP_TO_NEXT, true)
         } else {
-            extras.remove(MediaControlConstants.EXTRA_RESERVE_SLOT_SKIP_TO_NEXT)
+            extras.remove(EXTRA_RESERVE_SLOT_SKIP_TO_NEXT)
         }
     }
 }
